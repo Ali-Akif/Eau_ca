d9601f239db6b93c0081cf3b29af89bb97d57887
Ali-Akif <ali-akif@live.fr>
2023-12-28 22:39:37 +0100

    Create eau00.py

---
+++ eau00.py
@@ -0,0 +1,11 @@
+# COMBINATION OF 3 NUMBERS
+liste = []
+for a in range(10):
+    for b in range(a, 10):
+        for c in range(b, 10):
+            if a != b and a != c and b != c:
+                nombre_a_tester = f"{a}{b}{c}"
+                if nombre_a_tester not in liste:
+                    liste.append("".join(nombre_a_tester))
+
+print(", ".join(liste))
