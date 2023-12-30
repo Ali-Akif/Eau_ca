567e81a723b526c3b78d5bf1e02012840a941fbc
Ali-Akif <ali-akif@live.fr>
2023-12-30 17:29:20 +0100

    Update eau01.py
    Wip

--- eau01.py
+++ eau01.py
@@ -1,1 +1,11 @@
-#nothing, there was a lot of extra work hours today, I just don't want to lose the streak, I'll come back stronger. If you read this, give me money. Thanks. Goodnight.
+zero = 0
+
+for i in range(zero, 100):
+    for i in range(1,100):
+        print(f"{"0" if zero < 10 else ""}{zero} {"0" if i < 10 else ""}{i}", end=", ")
+
+    zero +=1
+    
+        
+
+    
