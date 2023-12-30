a258fa6bcbbe5d2a07a5cb0da1839d0905139a3b
Ali-Akif <ali-akif@live.fr>
2023-12-30 23:07:14 +0100

    Update eau01.py

--- eau01.py
+++ eau01.py
@@ -1,5 +1,9 @@
-for i in range(100):
-    for e in range(100):
-        print(f"{i:02d} {e:02d}")
+#COMBINATION OF 2 NUMBERS
 
-    
+liste =[]
+
+for i in range(100):
+    for e in range(i + 1,100):
+        liste.append(f"{i:02d} {e:02d}")
+
+print(", ".join(liste))
