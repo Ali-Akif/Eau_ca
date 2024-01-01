168d1010de09b81c1e08778eb865ecbff7392d13
Ali-Akif <ali-akif@live.fr>
2024-01-01 21:33:42 +0100

    Update eau02.py

--- eau02.py
+++ eau02.py
@@ -1,6 +1,14 @@
+# REVERSE TERMINAL ARGUMENTS
+
 import sys
 
+
+# Part 1 : Slicing
+
 terminal_arguments = sys.argv
+
+
+# Part 2 : Error Handling, Resolution and Display
 
 if len(terminal_arguments) < 2:
     print("erreur.")
