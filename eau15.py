0cb7a8fb533d3da0f100639c62f9ab5af1ffc140
Ali-Akif <ali-akif@live.fr>
2024-01-05 18:00:53 +0100

    Create eau15.py

---
+++ eau15.py
@@ -0,0 +1,11 @@
+# EAU OK
+
+
+# Part 1 : Resolution
+
+eau_moindset = "une dinguerie, j'ai beaucoup appris, sur mon organisation et ma façon d'apprendre, mais aussi sur le code."
+
+
+# Part 2 : Display
+
+print(f"J'ai terminé l'eau et c'étais {eau_moindset}")
