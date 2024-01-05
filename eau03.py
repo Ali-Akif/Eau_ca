467e3f73e4aa1caa161c3d3a1b557338dfc6f1f3
Ali-Akif <ali-akif@live.fr>
2024-01-05 18:00:27 +0100

    Update eau03.py

--- eau03.py
+++ eau03.py
@@ -1,16 +1,18 @@
+# FIBONACCI
+
 import sys
 
 
 # Part 1 : Function
 
-def fibonacci(n):
-    if n == 0:
+def fibonacci(given_int):
+    if given_int == 0:
         return 0
-    elif n == 1:
+    elif given_int == 1:
         return 1
 
     a, b = 0, 1
-    for i in range(n - 1):
+    for i in range(given_int - 1):
         a, b = b, a + b
 
     return b
@@ -18,11 +20,8 @@ def fibonacci(n):
 
 # Part 2 : Error Handling
 
-if len(sys.argv) != 2:
-    print("Erreur : Veuillez fournir un seul argument.")
-    exit()
-elif not sys.argv[1].isdigit():
-    print("Erreur : L'argument doit être un nombre entier positif.")
+if not (len(sys.argv) == 2 and sys.argv[1].isdigit()):
+    print("erreur")
     exit()
 
 
