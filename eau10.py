efa95c8d9f60fb9407800df83c0d2f698c954e9d
Ali-Akif <ali-akif@live.fr>
2024-01-05 18:00:43 +0100

    Update eau10.py

--- eau10.py
+++ eau10.py
@@ -3,22 +3,17 @@
 import sys
 
 
-# Part 1 : Error Handling
+# Part 1 : Error Handling and Slicing
 
 if sys.argv[1:]:
     chaine = sys.argv[1:]
+    last_word = chaine[-1]
+    chaine.pop()
 else:
     print("erreur")
     exit()
 
-
-# Part 2 : Slicing
-    
-last_word = chaine[-1]
-chaine.pop()
-
-
-# Part 3 : Resolution and Display
+# Part 2 : Resolution and Display
 
 if last_word in chaine:
     index_wanted = chaine.index(last_word)
