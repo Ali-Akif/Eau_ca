ebff901d389ef48399a3d83a610d7c8dee8ef9c9
Ali-Akif <ali-akif@live.fr>
2024-01-05 18:00:33 +0100

    Update eau05.py

--- eau05.py
+++ eau05.py
@@ -5,21 +5,15 @@ import sys
 
 # Part 1 : Error Handling
 
-if len(sys.argv) != 3:
-    print("erreur.")
-    exit()
-else:
-    a = sys.argv[1]
-    b = sys.argv[2]
-
-if a.isdigit() or b.isdigit():
+if len(sys.argv) != 3 and sys.argv[1].isdigit() and sys.argv[2].isdigit(): 
     print("erreur.")
     exit()
 
 
 # Slicing, Resolution and Display
-    
-verif = a.count(b)
+
+arg1, arg2 = sys.argv[1], sys.argv[2]
+verif = arg1.count(arg2)
 
 if verif == 1:
     print(True)
