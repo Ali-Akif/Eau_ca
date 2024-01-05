57fad6428472aac146ad4c453afc245d152bdd54
Ali-Akif <ali-akif@live.fr>
2024-01-05 18:00:29 +0100

    Update eau04.py

--- eau04.py
+++ eau04.py
@@ -5,9 +5,9 @@ import sys
 
 # Part 1 : Function
 
-def nombre_premier_superieur(n):
-    n = int(n)
-    for i in range(n + 1, n + 21):
+def nombre_premier_superieur(number_string):
+    number_int = int(number_string)
+    for i in range(number_int + 1, number_int + 21):
         for nombre in [2, 3, 4, 5, 6, 7, 8, 9]:
             if i % nombre == 0 and i != nombre:
                 break
